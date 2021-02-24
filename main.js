b66e358dbef070b248fffb2e1243a2c3ac9d64f7
Ahsan AUA <auawdigital@gmail.com>
2021-02-24 14:43:05 +0500

    some small adjustments to navbar

--- main.js
+++ main.js
@@ -3,4 +3,6 @@ const navbar = document.querySelector(".navbar-ul");
 
 burger.addEventListener("click", () => {
   navbar.classList.toggle("hidden");
+  burger.classList.toggle("active");
+  navbar.classList.toggle("nav-active");
 });
