52369d83c6a053be8e2e05b7ba486630f6aa5a67
Ahsan AUA <auawdigital@gmail.com>
2021-02-24 17:21:11 +0500

    simple navbar complete

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const burger = document.querySelector(".burger");
 const navbar = document.querySelector(".navbar-ul");
 
 burger.addEventListener("click", () => {
-  navbar.classList.toggle("hidden");
   burger.classList.toggle("active");
   navbar.classList.toggle("nav-active");
 });
