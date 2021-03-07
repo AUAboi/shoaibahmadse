d88e102402bfa08e3255bec9fa2aed65f403689b
Ahsan AUA <auawdigital@gmail.com>
2021-03-07 23:46:55 +0500

    line animation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 const burger = document.querySelector(".burger");
 const navbar = document.querySelector(".navbar-ul");
+const line = document.querySelectorAll(".line");
 
 burger.addEventListener("click", () => {
   burger.classList.toggle("active");
+  line.forEach((line) => {
+    line.classList.toggle("line-active");
+  });
   navbar.classList.toggle("nav-active");
 });
